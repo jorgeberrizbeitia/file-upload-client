307af4b79f85d1cf031265e5b8fe726a22588ccf
Jorge <jorgeberrizbeitia@gmail.com>
2021-04-10 19:22:25 +0200

    final code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+// App.js
+
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import AddThing from './components/AddThing'; // <== !!!
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AddThing />
+      </div>
+    );
+  }
 }
 
 export default App;
